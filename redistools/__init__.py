a29f43ae4af9aa448be1c8cd20285d1be6a0a7ee
wwqgtxx <wwqgtxx@gmail.com>
2016-12-22 20:36:28 +0800

    add the pypi's file

---
+++ redistools/__init__.py
@@ -0,0 +1,4 @@
+from .redistools import *
+from .redismanager import RedisManager
+
+__version__ = "0.1.0"
---
+++ setup.py
@@ -0,0 +1,40 @@
+from distutils.core import setup
+
+PACKAGE = "redistools"
+NAME = "redistools"
+DESCRIPTION = "A Redis tools provide python's Lock RLock Semaphore BoundedSemaphore Condition Event Barrier and Queue cross process."
+AUTHOR = "wwqgtxx"
+AUTHOR_EMAIL = "wwqgtxx@gmail.com"
+URL = "https://github.com/wwqgtxx/RedisTools"
+VERSION = __import__(PACKAGE).__version__
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    # long_description=read("README.md"),
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    license="GNU General Public License v3 (GPLv3)",
+    url=URL,
+    packages=["redistools"],
+    platforms='any',
+    install_requires=[
+        'future>=0.16.0',
+        'redis>=2.10.5',
+        'six>=1.10.0',
+        'redis-collections>=0.4.2'
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Topic :: Database',
+    ],
+    zip_safe=False,
+)
